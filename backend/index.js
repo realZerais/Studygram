cb1f34d9ce72672764ed93911f79774987a66658
zerais <davesiarez18@gmail.com>
2023-12-05 20:33:40 +0800

    sample setup on how the stack works

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const cors = require('cors'); // Import the cors middleware
+
 
 const app = express();
 const PORT = 3000;
 
 
 
+
 // Middleware to parse JSON requests
 app.use(express.json());
+
+app.use(cors()); // Enable CORS for all routes
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+
+// Set up PostgreSQL connection pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'studygram',
+  password: 'sasageyo',
+  port: 5432, // Change the port if your PostgreSQL server is running on a different port
+});
 
 
 
@@ -31,13 +47,63 @@ app.post('/test', (req, res) => {
   console.log('Received form data:', { name, age, gender });
 
   // Send a response (you can customize this based on your needs)
-  res.send(req.body.age);
+  res.send(req.body);
 
 });
 
+// Test the database connection
+pool.query('SELECT * from tasks', (err, res) => {
+  if (err) {
+      console.error('Error connecting to the database:', err);
+  } else {
+      console.log(res.rows);
+  }
+});
+
+//test add material
+
+
+// Endpoint to get all tasks
+app.get('/tasks', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM tasks');
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+// Endpoint to add a new task
+app.post('/tasks', async (req, res) => {
+  const { description } = req.body;
+  try {
+    await pool.query('INSERT INTO tasks (description) VALUES ($1)', [description]);
+    res.status(201).send('Task added successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Endpoint to delete a task
+app.delete('/tasks/:id', async (req, res) => {
+  const taskId = req.params.id;
+  try {
+    await pool.query('DELETE FROM tasks WHERE id = $1', [taskId]);
+    res.send('Task deleted successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 app.listen(
     PORT, 
     () => {console.log(`Server is running on http://localhost:${PORT}`)
 });
+
+
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,48 @@
-<main>
-
-</main>
+<script>
+    import { onMount } from 'svelte';
+    import axios from 'axios';
+  
+    let tasks = [];
+  
+    onMount(async () => {
+      // Fetch tasks from the backend on component mount
+      const response = await axios.get('http://localhost:3000/tasks');
+      tasks = response.data;
+    });
+  
+    // Function to add a new task
+    async function addTask() {
+      const description = prompt('Enter task description:');
+      if (description) {
+        await axios.post('http://localhost:3000/tasks', { description });
+        // Refresh tasks after adding
+        const response = await axios.get('http://localhost:3000/tasks');
+        tasks = response.data;
+      }
+    }
+  
+    // Function to delete a task
+    async function deleteTask(id) {
+      if (confirm('Are you sure you want to delete this task?')) {
+        await axios.delete(`http://localhost:3000/tasks/${id}`);
+        // Refresh tasks after deletion
+        const response = await axios.get('http://localhost:3000/tasks');
+        tasks = response.data;
+      }
+    }
+  </script>
+  
+  <main>
+    <h1>Task Management</h1>
+  
+    <ul>
+      {#each tasks as task (task.id)}
+        <li on:click={() => deleteTask(task.id)}>
+          {task.description}
+        </li>
+      {/each}
+    </ul>
+  
+    <button on:click={addTask}>Add Task</button>
+  </main>
+  
