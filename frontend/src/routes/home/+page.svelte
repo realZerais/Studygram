db1d367598149e7a1dbeeab629cf2072ddc65ebb
zerais <davesiarez18@gmail.com>
2023-12-28 00:13:37 +0800

    frontend: upload form bruh kill me

--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -9,32 +9,93 @@
 
     let materials = [];
 
+    const nowDate = () =>{
+        const currentDate = new Date();
+
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const day = String(currentDate.getDate()).padStart(2, '0')
+        return `${year}-${month}-${day}`;
+    }
+        
+    let title = '';
+    let content = '';    
+    let date = nowDate();
+
+    // console.log(nowDate)
+
+
     onMount(async () => {
-        const response = await fetch(materialsAPI);
-        materials = await response.json();
-        console.log(materials);
+        try {
+            const response = await fetch(materialsAPI);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            materials = await response.json();
+            console.log(materials);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     });
+
+
+
+    const handleSubmit = async () =>{
+
+        const formData = JSON.stringify([
+            {
+                title,
+                date,
+                content,
+            },
+        ]);
+
+        console.log(formData);
+        
+        try {   
+            const response = await fetch('http://localhost:3000/materials', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: formData,
+            }); 
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            // Handle the response from the server
+            console.log('Server Response:', data);
+
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+
+        
+       
+    }
+
     
-   
 
 </script>
 
 <main class="flex flex-col justify-center items-center mt-2 gap-10">
     
-    <div class="h-[40vh] w-[60%] flex flex-col gap-5 justify-center items-center border-2 rounded-md">
+    <form on:submit|preventDefault={handleSubmit} class="h-[40vh] w-[60%] flex flex-col gap-5 justify-center items-center border-black border-2 rounded-md">
         <h1>UPLOAD MATERIAL</h1>
-        <h2>Material Caption:</h2>
-        <input type="text">
-
-        <form action="/action_page.php">
-            <input type="file" id="myFile" name="filename">
-            <input type="submit">
-        </form>
-    </div>
+        <p>Material TITLE:</p>
+        <!-- <p>{title}</p> -->
+        <input type="text" name="title" bind:value={title} required class="h-[20%]  border-2 rounded-sm border-black ">
+        <input type="text" name="content" bind:value={content} placeholder="What's on your mind?" class="h-[50%] w-[70%] border-2 rounded-sm border-black ">
+        <button>UPLOAD</button>
+    </form>
 
     <h1>FEED</h1>
 
-    <div class="w-[60%] flex flex-col gap-20 justify-center items-center border-2 rounded-md p-6">
+    <div class="w-[60%] flex flex-col gap-20 justify-center items-center border-black border-2 rounded-md p-6 m-6">
        
         
             {#each materials as material}
